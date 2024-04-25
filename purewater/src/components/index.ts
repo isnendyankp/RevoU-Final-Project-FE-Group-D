c5c50c4414a082ac77be55fefb561c14838a7baa
Afifpip <afif.firfik@gmail.com>
2024-04-25 21:00:18 +0700

    NavBar component

--- purewater/src/components/index.ts
+++ purewater/src/components/index.ts
@@ -4,3 +4,4 @@ export { default as Card } from './Card';
 export { default as Text } from './Text';
 export { default as Footer } from './Footer';
 export { default as Button } from './Button';
+export { default as Navbar } from './Navbar';
