35911d26a567f481c98cf6a38190ef85b1b5cd43
isnendyankp <isnendyankp@gmail.com>
2024-04-29 20:19:40 +0700

    Add login page

--- purewater/src/pages/login/index.tsx
+++ purewater/src/pages/login/index.tsx
@@ -1,17 +1,13 @@
 import React from 'react';
 import Login from '../../components/Login';
-import Layout from '../../layout';
 
 const LoginPage = () => {
 
     return (
         <>
-            <Layout>
             
                 <Login />
-            
-            </Layout>
-
+        
         </>
     );
 };
