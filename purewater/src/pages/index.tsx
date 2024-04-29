02b86a2f1f2fcf125eb5f402bd0001d9628051e9
isnendyankp <isnendyankp@gmail.com>
2024-04-29 20:06:31 +0700

    Fix FAQ

--- purewater/src/pages/index.tsx
+++ purewater/src/pages/index.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import { Carousel, FAQ } from "@/components";
+import { Carousel, FAQ } from '@/components';
 import 'flowbite';
+// import Image from "next/image";
+// import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
