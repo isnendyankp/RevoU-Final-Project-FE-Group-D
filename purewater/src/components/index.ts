9861e405b32fbd81ade3be8ef49cdc60ca765f80
Afifpip <afif.firfik@gmail.com>
2024-04-30 21:21:55 +0700

    Why Us component

--- purewater/src/components/index.ts
+++ purewater/src/components/index.ts
@@ -5,6 +5,7 @@ export { default as Text } from './Text';
 export { default as Footer } from './Footer';
 export { default as Button } from './Button';
 export { default as Navbar } from './Navbar';
-export { default as Carousel } from './Carousel';
+export { default as Carousels } from './Carousel';
 export { default as FAQ } from './FAQ';
 export { default as AboutUs } from './About';
+export { default as WhyUs } from './Why'
