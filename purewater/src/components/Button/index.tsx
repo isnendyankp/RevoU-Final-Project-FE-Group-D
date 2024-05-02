9f48e8d8a2ddc970b66b5a661111b73ed2e384e3
isnendyankp <isnendyankp@gmail.com>
2024-05-02 21:23:36 +0700

    fixing centering button login page

--- purewater/src/components/Button/index.tsx
+++ purewater/src/components/Button/index.tsx
@@ -8,7 +8,7 @@ interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
 const Button = ({ label, ...props }: Props) => {
 
   return (
-    <button {...props} className={`${props.className} flex items-center justify-between mb-4 w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500`}
+    <button {...props} className={`${props.className} flex items-center mb-4 w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500`}
     >{label}</button>
   )
 }
