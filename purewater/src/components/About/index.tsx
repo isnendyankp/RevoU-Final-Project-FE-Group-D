af8f853129d83805cab869c3278a5c09d54e0ad2
Afifpip <afif.firfik@gmail.com>
2024-05-07 19:57:28 +0700

    update anchor link navbar

--- purewater/src/components/About/index.tsx
+++ purewater/src/components/About/index.tsx
@@ -1,6 +1,6 @@
 const AboutUs = () => {
     return (
-      <div className=" isolate overflow-hidden bg-white py-24 sm:py-32">
+      <div id="aboutus" className=" isolate overflow-hidden bg-white py-24 sm:py-32">
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
           <div className="mx-auto max-w-2xl lg:mx-0">
             <h2 className="text-4xl font-bold tracking-tight text-black sm:text-6xl">About us</h2>
